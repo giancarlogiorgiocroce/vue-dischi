ae239092277f9d2f7bbb12ee9c8ea0c25a6ef260
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-17 16:51:19 +0200

    Importata l'API e iniziato il ciclo

---
+++ vue-dischi/src/components/MainComp.vue
@@ -0,0 +1,52 @@
+<template>
+  <main>
+      <div class="container">
+          <div class="row debug">
+              <SingleCard 
+                v-for="(el, i) in apiArray"
+                :key="i"/>
+          </div>
+      </div>
+  </main>
+</template>
+
+<script>
+import SingleCard from './SingleCard.vue';
+import axios from 'axios';
+
+export default {
+    components: { SingleCard },
+    data(){
+        return{
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            apiArray: {},
+        }
+    },
+    mounted(){
+        this.callApi();
+    },
+    methods:{
+        callApi(){
+            axios.get(this.apiUrl)
+            .then((res)=>{
+                this.apiArray = res.data.response;
+                console.log(this.apiArray);
+                // console.log(res.data.response);
+            })
+            .catch((e)=>{
+                return e;
+            })
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/vars';
+main{
+    background-color: $primary-color;
+    min-height: calc(100vh - 75px);
+    padding: 5vh;
+
+}
+</style>
---
+++ vue-dischi/src/components/SingleCard.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="debug">
+    <div class="image">
+        <img alt="ughide">
+    </div>
+    <div class="title">
+        Ugolandia
+    </div>
+    <div class="author">
+        <!-- {{album.author}} -->Ugo
+    </div>
+</div>  
+</template>
+
+<script>
+export default {
+// props:{
+//     album: Object,
+// }
+}
+</script>
+
+<style scoped lang="scss">
+div{
+    color:white;
+    width: calc(100% / 5 - 2vw);
+    margin: 1vw;
+}
+</style>
