80591154f2955369bfdfb0b59c9920beee363888
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-17 15:45:31 +0200

    Header completato

--- vue-dischi/src/App.vue
+++ vue-dischi/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    HW
+    <HeaderComp />
   </div>
 </template>
 
 <script>
+import HeaderComp from './components/HeaderComp.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    HeaderComp
+}
 }
 </script>
 
 <style lang="scss">
+@import 'assets/styles/general.scss';
+@import '~bootstrap/scss/bootstrap.scss';
+
 
 </style>
