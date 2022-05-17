6cb31867276d4b91c6d114b6ec878d43e77e8290
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-17 15:20:30 +0200

    Setup di base

---
+++ vue-dischi/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    HW
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
